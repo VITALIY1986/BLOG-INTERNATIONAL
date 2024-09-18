fc2dd958068e71f471ca18a9d7ee88a9ca4fc8a6
VITALIY1986 <staslego1986@gmail.com>
2024-09-18 15:40:41 +0300

    hffdgkfgfkd

--- components/post-preview.js
+++ components/post-preview.js
@@ -1,31 +1,35 @@
-import Link from 'next/link'
-import Avatar from '../components/avatar'
-import DateComponent from '../components/date'
-import CoverImage from './cover-image'
+import Link from 'next/link';
+import Avatar from '../components/avatar';
+import DateComponent from '../components/date';
+import CoverImage from './cover-image';
 
 export default function PostPreview({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
+  title = 'Untitled Post',  // Значение по умолчанию
+  coverImage = { url: '/default-image.jpg' },  // URL изображения по умолчанию
+  date = '1970-01-01',  // Дата по умолчанию
+  excerpt = 'No excerpt available.',  // Описание по умолчанию
+  author = { name: 'Anonymous', picture: '/default-avatar.jpg' },  // Автор по умолчанию
+  slug = '#',
 }) {
   return (
     <div>
       <div className="mb-5">
-        <CoverImage title={title} slug={slug} url={coverImage.url} />
+        <CoverImage title={title} slug={slug} url={coverImage?.url || '/default-image.jpg'} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`} className="hover:underline">
-          {title}
+          {title || 'Untitled Post'}
         </Link>
       </h3>
       <div className="text-lg mb-4">
-        <DateComponent dateString={date} />
+        <DateComponent dateString={date || '1970-01-01'} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      {author && <Avatar name={author.name} picture={author.picture} />}
+      <p className="text-lg leading-relaxed mb-4">
+        {excerpt || 'No excerpt available.'}
+      </p>
+      {author && (
+        <Avatar name={author?.name || 'Anonymous'} picture={author?.picture || '/default-avatar.jpg'} />
+      )}
     </div>
-  )
+  );
 }
